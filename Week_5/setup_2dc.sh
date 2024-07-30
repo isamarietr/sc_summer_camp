682b3738b546410a7e3ad8198d0d79b88a3bd1cb
Isa Torres <isa.torres@gmail.com>
2024-07-30 18:46:09 -0400

    5 output

--- Week_5/setup_2dc.sh
+++ Week_5/setup_2dc.sh
@@ -4,9 +4,9 @@
 
 # Variables
 BASE_VM_NAME="base_ubuntu"
-PASSWORD=`cat ./password.txt`
+PASSWORD=`cat ./PASSWORD.txt`
 
-MDB_MACHINES=("mdb1" "mdb2" "mdb3" "mdb4" "mdb5" "mdb6")
+MDB_MACHINES=("dc11" "dc12" "dc13" "dc21" "dc22" "dc23")
 
 echo "Creating AppDB Machines..."
 for NEW_VM in "${MDB_MACHINES[@]}"
@@ -24,10 +24,10 @@ do
 mdb_ip=$(VBoxManage guestproperty get $MDB_VM "/VirtualBox/GuestInfo/Net/0/V4/IP" | awk '{ print $2 }')
 
 echo "Sending mongod.config..."
-sshpass -p $password scp -o StrictHostKeyChecking=no ./mongod.conf ubuntu@$mdb_ip:~/mongod.conf
+sshpass -p $PASSWORD scp -o StrictHostKeyChecking=no ./mongod.conf ubuntu@$mdb_ip:~/mongod.conf
 
 echo "Installing MongoDB..."
-sshpass -p $password ssh -o StrictHostKeyChecking=no ubuntu@$mdb_ip << EOF
+sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no ubuntu@$mdb_ip << EOF
 
     # get mongodb EA binaries and install
     sudo apt-get install -y gnupg curl
@@ -38,12 +38,12 @@ sshpass -p $password ssh -o StrictHostKeyChecking=no ubuntu@$mdb_ip << EOF
     echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.com/apt/ubuntu jammy/mongodb-enterprise/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-enterprise-7.0.list
 
     sudo apt-get update 
-    sudo apt-get install -y mongodb-enterprise
+    sudo apt-get install -y mongodb-database-tools mongodb-mongosh  mongodb-enterprise 
 
     sudo chown ubuntu /etc/mongod.conf
     sudo cp ~/mongod.conf /etc/mongod.conf
 
-    sudo mount /dev/vdb /data
+    sudo mkdir /data
     sudo chown -R mongodb:mongodb /data
 
     sudo openssl rand -base64 756 > ~/mongodb.key
@@ -63,18 +63,18 @@ done
 
 echo "Done setting up all AppDB VMS!"
 
-ip1=$(VBoxManage guestproperty get mdb1 "/VirtualBox/GuestInfo/Net/0/V4/IP" | awk '{ print $2 }')
-ip2=$(VBoxManage guestproperty get mdb2 "/VirtualBox/GuestInfo/Net/0/V4/IP" | awk '{ print $2 }')
-ip3=$(VBoxManage guestproperty get mdb3 "/VirtualBox/GuestInfo/Net/0/V4/IP" | awk '{ print $2 }')
-ip4=$(VBoxManage guestproperty get mdb4 "/VirtualBox/GuestInfo/Net/0/V4/IP" | awk '{ print $2 }')
-ip5=$(VBoxManage guestproperty get mdb5 "/VirtualBox/GuestInfo/Net/0/V4/IP" | awk '{ print $2 }')
-ip6=$(VBoxManage guestproperty get mdb6 "/VirtualBox/GuestInfo/Net/0/V4/IP" | awk '{ print $2 }')
+ip1=$(VBoxManage guestproperty get dc11 "/VirtualBox/GuestInfo/Net/0/V4/IP" | awk '{ print $2 }')
+ip2=$(VBoxManage guestproperty get dc12 "/VirtualBox/GuestInfo/Net/0/V4/IP" | awk '{ print $2 }')
+ip3=$(VBoxManage guestproperty get dc13 "/VirtualBox/GuestInfo/Net/0/V4/IP" | awk '{ print $2 }')
+ip4=$(VBoxManage guestproperty get dc21 "/VirtualBox/GuestInfo/Net/0/V4/IP" | awk '{ print $2 }')
+ip5=$(VBoxManage guestproperty get dc22 "/VirtualBox/GuestInfo/Net/0/V4/IP" | awk '{ print $2 }')
+ip6=$(VBoxManage guestproperty get dc23 "/VirtualBox/GuestInfo/Net/0/V4/IP" | awk '{ print $2 }')
 
 MDB_URI="mongodb://$ip1:27017,$ip2:27017,$ip3:27017,$ip4:27017,$ip5:27017,$ip6:27017" 
 echo "Connection String:"
 echo $MDB_URI
 echo "Initiating RS..."
-# mongosh $ip1 --eval 'use admin; db.createUser({user: "adminUser", pwd: "password123", roles: [{role: "userAdminAnyDatabase", db: "admin"}, "readWriteAnyDatabase"]})'
+# mongosh $ip1 --eval 'use admin; db.createUser({user: "adminUser", pwd: "PASSWORD123", roles: [{role: "userAdminAnyDatabase", db: "admin"}, "readWriteAnyDatabase"]})'
 INIT_CMD="rs.initiate({_id: 'rs0', members: [{ _id: 0, host: '$ip1:27017' },{ _id: 1, host: '$ip2:27017' },{ _id: 2, host: '$ip3:27017' },{ _id: 3, host: '$ip4:27017' },{ _id: 4, host: '$ip5:27017'},{ _id: 5, host: '$ip6:27017', priority: 0, hidden: true }]})"
 mongosh  $MDB_URI --eval  $INIT_CMD 
 
@@ -83,9 +83,9 @@ mongosh  $MDB_URI --eval  "rs.status()"
 sleep 2
 
 echo "Sutting down first 3 nodes..."
-VBoxManage controlvm mdb1 acpipowerbutton
-VBoxManage controlvm mdb2 acpipowerbutton
-VBoxManage controlvm mdb3 acpipowerbutton
+VBoxManage controlvm dc11 acpipowerbutton
+VBoxManage controlvm dc12 acpipowerbutton
+VBoxManage controlvm dc13 acpipowerbutton
 
 RECONFIG_CMD="var cfg = rs.conf(); cfg.members = [{ _id: 3, host: '$ip4:27017' },{ _id: 4, host: '$ip5:27017'},{ _id: 5, host: '$ip6:27017'}]; rs.reconfig(cfg, { force: true }); "
 mongosh  $MDB_URI --eval  $RECONFIG_CMD
